cc2db1912e4bfa695965d3b53829865342dbe1d1
Ramona Comanescu <com.ramona@yahoo.com>
2017-01-14 20:54:53 +0000

    added first instrumented test

--- app/src/androidTest/java/com/example/rama/androidtut/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/rama/androidtut/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.rama.grabble", appContext.getPackageName());
+        assertEquals("com.example.rama.androidtut", appContext.getPackageName());
     }
 }
---
+++ app/src/androidTest/java/com/example/rama/androidtut/MainActivityButtonsInstrumentedTest.java
@@ -0,0 +1,58 @@
+package com.example.rama.androidtut;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityButtonsInstrumentedTest {
+    @Rule
+    public ActivityTestRule mActivityTestRule=new ActivityTestRule<>(MainActivity.class);
+
+    /**
+     * Test pressing help button, which displays instructions
+     */
+    @Test
+    public void showInstructions(){
+        onView(withId(R.id.help)).perform(click());
+    }
+    @Test
+    public void signIn(){
+        onView(withId(R.id.email_sign_in_button)).check(matches(allOf( isEnabled(), isClickable()))).perform(
+                new ViewAction() {
+                    @Override
+                    public Matcher<View> getConstraints() {
+                        return isEnabled(); // no constraints, they are checked above
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return "click plus button";
+                    }
+
+                    @Override
+                    public void perform(UiController uiController, View view) {
+                        view.performClick();
+                    }
+                }
+        );
+    }
+}
+
